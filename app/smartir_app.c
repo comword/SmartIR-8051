0a717d2fdaef2849c3d7ea0e99ea7066655db3b3
henorvell <comword@live.com>
2016-08-02 13:34:07 +0800

    add zstack app template

---
+++ app/smartir_app.c
@@ -0,0 +1,224 @@
+#include "ZComDef.h"
+#include "OSAL.h"
+#include "AF.h"
+#include "ZDApp.h"
+#include "ZDObject.h"
+#include "ZDProfile.h"
+#include "MT_SYS.h"
+#include "MT_UART.h"
+
+#include "zcl.h"
+#include "zcl_general.h"
+#include "zcl_ha.h"
+#include "zcl_ezmode.h"
+
+#if defined (OTA_CLIENT) && (OTA_CLIENT == TRUE)
+#include "zcl_ota.h"
+#include "hal_ota.h"
+#endif
+
+#include "smartir_app.h"
+#include "string.h"
+
+byte zclSmartIR_TaskID;
+uint8 zclSmartIRSeqNum;
+devStates_t zclSmartIR_NwkState = DEV_INIT;
+extern byte App_TaskID;
+#ifdef ZCL_ON_OFF
+afAddrType_t zclSmartIR_DstAddr;
+#endif
+
+#ifdef ZCL_EZMODE
+static void zclSmartIR_ProcessZDOMsgs( zdoIncomingMsg_t *pMsg );
+static void zclSmartIR_EZModeCB( zlcEZMode_State_t state, zclEZMode_CBData_t *pData );
+
+static const zclEZMode_RegisterData_t zclSmartIR_RegisterEZModeData =
+{
+  &zclSmartIR_TaskID,
+  SMARTIR_EZMODE_NEXTSTATE_EVT,
+  SMARTIR_EZMODE_TIMEOUT_EVT,
+  &zclSmartIRSeqNum,
+  zclSmartIR_EZModeCB
+};
+
+// NOT ZLC_EZMODE, Use EndDeviceBind
+#else
+
+static cId_t bindingOutClusters[] =
+{
+  ZCL_CLUSTER_ID_GEN_ON_OFF
+};
+#define ZCLSMARTIR_BINDINGLIST   (sizeof(bindingOutClusters)/sizeof(bindingOutClusters[0]))
+#endif  // ZLC_EZMODE
+
+static endPointDesc_t SmartIR_TestEp =
+{
+  SMARTIR_ENDPOINT,                  // endpoint
+  &zclSmartIR_TaskID,
+  (SimpleDescriptionFormat_t *)NULL,  // No Simple description for this test endpoint
+  (afNetworkLatencyReq_t)0            // No Network Latency req
+};
+
+static uint8 aProcessCmd[] = { 1, 0, 0, 0 }; // used for reset command, { length + cmd0 + cmd1 + data }
+
+static void zclSmartIR_HandleKeys( byte shift, byte keys );
+static void zclSmartIR_BasicResetCB( void );
+static void zclSmartIR_IdentifyCB( zclIdentify_t *pCmd );
+static void zclSmartIR_IdentifyQueryRspCB(  zclIdentifyQueryRsp_t *pRsp );
+static void zclSmartIR_ProcessIdentifyTimeChange( void );
+static void zclSmartIR_ProcessIncomingMsg( zclIncomingMsg_t *msg );
+#ifdef ZCL_READ
+static uint8 zclSmartIR_ProcessInReadRspCmd( zclIncomingMsg_t *pInMsg );
+#endif
+#ifdef ZCL_WRITE
+static uint8 zclSmartIR_ProcessInWriteRspCmd( zclIncomingMsg_t *pInMsg );
+#endif
+static uint8 zclSmartIR_ProcessInDefaultRspCmd( zclIncomingMsg_t *pInMsg );
+#ifdef ZCL_DISCOVER
+static uint8 zclSmartIR_ProcessInDiscCmdsRspCmd( zclIncomingMsg_t *pInMsg );
+static uint8 zclSmartIR_ProcessInDiscAttrsRspCmd( zclIncomingMsg_t *pInMsg );
+static uint8 zclSmartIR_ProcessInDiscAttrsExtRspCmd( zclIncomingMsg_t *pInMsg );
+#endif
+
+static zclGeneral_AppCallbacks_t zclSmartIR_CmdCallbacks =
+{
+  zclSmartIR_BasicResetCB,               // Basic Cluster Reset command
+  zclSmartIR_IdentifyCB,                 // Identify command
+#ifdef ZCL_EZMODE
+  NULL,                                   // Identify EZ-Mode Invoke command
+  NULL,                                   // Identify Update Commission State command
+#endif
+  NULL,                                   // Identify Trigger Effect command
+  zclSmartIR_IdentifyQueryRspCB,          // Identify Query Response command
+  NULL,                                   // On/Off cluster commands
+  NULL,                                   // On/Off cluster enhanced command Off with Effect
+  NULL,                                   // On/Off cluster enhanced command On with Recall Global Scene
+  NULL,                                   // On/Off cluster enhanced command On with Timed Off
+#ifdef ZCL_LEVEL_CTRL
+  NULL,                                   // Level Control Move to Level command
+  NULL,                                   // Level Control Move command
+  NULL,                                   // Level Control Step command
+  NULL,                                   // Level Control Stop command
+#endif
+#ifdef ZCL_GROUPS
+  NULL,                                   // Group Response commands
+#endif
+#ifdef ZCL_SCENES
+  NULL,                                   // Scene Store Request command
+  NULL,                                   // Scene Recall Request command
+  NULL,                                   // Scene Response command
+#endif
+#ifdef ZCL_ALARMS
+  NULL,                                   // Alarm (Response) commands
+#endif
+#ifdef SE_UK_EXT
+  NULL,                                   // Get Event Log command
+  NULL,                                   // Publish Event Log command
+#endif
+  NULL,                                   // RSSI Location command
+  NULL                                    // RSSI Location Response command
+};
+
+#if defined (OTA_CLIENT) && (OTA_CLIENT == TRUE)
+#define DEVICE_POLL_RATE                 8000   // Poll rate for end device
+#endif
+
+void MY_UART_Init()
+{
+  halUARTCfg_t uartConfig;
+  App_TaskID = 0;
+  uartConfig.configured           = TRUE;
+  uartConfig.baudRate             = HAL_UART_BR_115200;
+  uartConfig.flowControl          = false;
+  uartConfig.flowControlThreshold = MT_UART_DEFAULT_THRESHOLD;
+  uartConfig.rx.maxBufSize        = MT_UART_DEFAULT_MAX_RX_BUFF;
+  uartConfig.tx.maxBufSize        = MT_UART_DEFAULT_MAX_TX_BUFF;
+  uartConfig.idleTimeout          = MT_UART_DEFAULT_IDLE_TIMEOUT;
+  uartConfig.intEnable            = TRUE;
+#if defined (ZTOOL_P1) || defined (ZTOOL_P2)
+  uartConfig.callBackFunc         = MT_UartProcessZToolData;
+#elif defined (ZAPP_P1) || defined (ZAPP_P2)
+  uartConfig.callBackFunc         = MT_UartProcessZAppData;
+#else
+  uartConfig.callBackFunc         = NULL;
+#endif
+#if defined (MT_UART_DEFAULT_PORT)
+  HalUARTOpen (MT_UART_DEFAULT_PORT, &uartConfig);
+#else
+  (void)uartConfig;
+#endif
+#if defined (ZAPP_P1) || defined (ZAPP_P2)
+  MT_UartMaxZAppBufLen  = 1;
+  MT_UartZAppRxStatus   = MT_UART_ZAPP_RX_READY;
+#endif
+}
+
+void zclSmartIR_Init( byte task_id )
+{
+  zclSmartIR_TaskID = task_id;
+  MY_UART_Init();
+  MT_UartRegisterTaskID(task_id);
+#ifdef ZCL_ON_OFF
+  // Set destination address to indirect
+  zclSmartIR_DstAddr.addrMode = (afAddrMode_t)AddrNotPresent;
+  zclSmartIR_DstAddr.endPoint = 0;
+  zclSmartIR_DstAddr.addr.shortAddr = 0;
+#endif
+  // This app is part of the Home Automation Profile
+  zclHA_Init( &zclSmartIR_SimpleDesc );
+  // Register the ZCL General Cluster Library callback functions
+  zclGeneral_RegisterCmdCallbacks( SMARTIR_ENDPOINT, &zclSmartIR_CmdCallbacks );
+  // Register the application's attribute list
+  zcl_registerAttrList( SMARTIR_ENDPOINT, SMARTIR_MAX_ATTRIBUTES, zclSmartIR_Attrs );
+  // Register the Application to receive the unprocessed Foundation command/response messages
+  zcl_registerForMsg( zclSmartIR_TaskID );
+#ifdef ZCL_EZMODE
+  // Register EZ-Mode
+  zcl_RegisterEZMode( &zclSmartIR_RegisterEZModeData );
+  // Register with the ZDO to receive Match Descriptor Responses
+  ZDO_RegisterForZDOMsg(task_id, Match_Desc_rsp);
+#endif
+  // Register for all key events - This app will handle all key events
+  RegisterForKeys( zclSmartIR_TaskID );
+  // Register for a test endpoint
+  afRegister( &SmartIR_TestEp );
+  ZDO_RegisterForZDOMsg( zclSmartIR_TaskID, End_Device_Bind_rsp );
+  ZDO_RegisterForZDOMsg( zclSmartIR_TaskID, Match_Desc_rsp );
+#if defined (OTA_CLIENT) && (OTA_CLIENT == TRUE)
+  // Register for callback events from the ZCL OTA
+  zclOTA_Register(zclSmartIR_TaskID);
+#endif
+  uint8 *Init_finished = "Welcome to Z-Stack Home (1.2.2a.44539)\n";
+  HalUARTWrite(0,Init_finished,strlen((const char*)Init_finished));
+}
+
+uint16 zclSmartIR_event_loop( uint8 task_id, uint16 events )
+{
+  afIncomingMSGPacket_t *MSGpkt;
+  (void)task_id;  // Intentionally unreferenced parameter
+  
+  return (events ^ SYS_EVENT_MSG);
+}
+
+void zclSmartIR_EZModeCB( zlcEZMode_State_t state, zclEZMode_CBData_t *pData )
+{
+  
+}
+
+static void zclSampleSw_HandleKeys( byte shift, byte keys )
+{
+
+}
+
+static void zclSmartIR_BasicResetCB( void )
+{
+  
+}
+static void zclSmartIR_IdentifyCB( zclIdentify_t *pCmd )
+{
+  
+}
+static void zclSmartIR_IdentifyQueryRspCB(  zclIdentifyQueryRsp_t *pRsp )
+{
+  
+}
---
+++ app/smartir_app.h
@@ -0,0 +1,196 @@
+
+extern void zclSmartIR_Init( byte task_id );
+extern void MY_UART_Init(void);
+extern UINT16 zclSmartIR_event_loop( byte task_id, UINT16 events );
+
+#define SMARTIR_ENDPOINT           8
+
+#define SMARTIR_MAX_ATTRIBUTES     15
+
+// Events for the sample app
+#define SMARTIR_IDENTIFY_TIMEOUT_EVT         0x0001
+#define SMARTIR_POLL_CONTROL_TIMEOUT_EVT     0x0002
+#define SMARTIR_EZMODE_TIMEOUT_EVT           0x0004
+#define SMARTIR_EZMODE_NEXTSTATE_EVT         0x0008
+#define SMARTIR_MAIN_SCREEN_EVT              0x0010
+
+#define SMARTIR_DEVICE_VERSION     0
+#define SMARTIR_FLAGS              0
+
+#define SMARTIR_HWVERSION          0
+#define SMARTIR_ZCLVERSION         0
+
+const cId_t zclSmartIR_InClusterList[] =
+{
+  ZCL_CLUSTER_ID_GEN_BASIC,
+  ZCL_CLUSTER_ID_GEN_IDENTIFY
+};
+#define ZCLSMARTIR_MAX_INCLUSTERS (sizeof(zclSmartIR_InClusterList) / sizeof( zclSmartIR_InClusterList[0]))
+
+const cId_t zclSmartIR_OutClusterList[] =
+{
+//  ZCL_CLUSTER_ID_GEN_ON_OFF,
+  ZCL_CLUSTER_ID_GEN_IDENTIFY,
+#ifdef ZCL_LEVEL_CTRL
+  ZCL_CLUSTER_ID_GEN_LEVEL_CONTROL,
+#endif
+  0
+};
+
+#define ZCLSMARTIR_MAX_OUTCLUSTERS    ( ( sizeof( zclSmartIR_OutClusterList ) / sizeof( zclSmartIR_OutClusterList[0] ) ) - 1 )
+
+SimpleDescriptionFormat_t zclSmartIR_SimpleDesc =
+{
+  SMARTIR_ENDPOINT,                   //  int Endpoint;
+  ZCL_HA_PROFILE_ID,                  //  uint16 AppProfId[2];
+  ZCL_HA_DEVICEID_REMOTE_CONTROL,     //  uint16 AppDeviceId[2];
+  SMARTIR_DEVICE_VERSION,             //  int   AppDevVer:4;
+  SMARTIR_FLAGS,                      //  int   AppFlags:4;
+  ZCLSMARTIR_MAX_INCLUSTERS,          //  byte  AppNumInClusters;
+  (cId_t *)zclSmartIR_InClusterList,  //  byte *pAppInClusterList;
+  ZCLSMARTIR_MAX_OUTCLUSTERS,         //  byte  AppNumInClusters;
+  (cId_t *)zclSmartIR_OutClusterList  //  byte *pAppInClusterList;
+};
+
+const uint8 zclSmartIR_HWRevision = SMARTIR_HWVERSION;
+const uint8 zclSmartIR_ZCLVersion = SMARTIR_ZCLVERSION;
+const uint8 zclSmartIR_ManufacturerName[] = { 16, 'T','e','x','a','s','I','n','s','t','r','u','m','e','n','t','s' };
+const uint8 zclSmartIR_ModelId[] = { 16, 'T','I','0','0','0','1',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ' };
+const uint8 zclSmartIR_DateCode[] = { 16, '2','0','0','6','0','8','3','1',' ',' ',' ',' ',' ',' ',' ',' ' };
+const uint8 zclSmartIR_PowerSource = POWER_SOURCE_MAINS_1_PHASE;
+
+uint8 zclSmartIR_LocationDescription[17] = { 16, ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ' };
+uint8 zclSmartIR_PhysicalEnvironment = 0;
+uint8 zclSmartIR_DeviceEnable = DEVICE_ENABLED;
+
+// Identify Cluster
+uint16 zclSmartIR_IdentifyTime = 0;
+
+CONST zclAttrRec_t zclSmartIR_Attrs[SMARTIR_MAX_ATTRIBUTES] =
+{
+  // *** General Basic Cluster Attributes ***
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,             // Cluster IDs - defined in the foundation (ie. zcl.h)
+    {  // Attribute record
+      ATTRID_BASIC_HW_VERSION,            // Attribute ID - Found in Cluster Library header (ie. zcl_general.h)
+      ZCL_DATATYPE_UINT8,                 // Data Type - found in zcl.h
+      ACCESS_CONTROL_READ,                // Variable access control - found in zcl.h
+      (void *)&zclSmartIR_HWRevision  // Pointer to attribute variable
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_ZCL_VERSION,
+      ZCL_DATATYPE_UINT8,
+      ACCESS_CONTROL_READ,
+      (void *)&zclSmartIR_ZCLVersion
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_MANUFACTURER_NAME,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ,
+      (void *)zclSmartIR_ManufacturerName
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_MODEL_ID,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ,
+      (void *)zclSmartIR_ModelId
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_DATE_CODE,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ,
+      (void *)zclSmartIR_DateCode
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_POWER_SOURCE,
+      ZCL_DATATYPE_UINT8,
+      ACCESS_CONTROL_READ,
+      (void *)&zclSmartIR_PowerSource
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_LOCATION_DESC,
+      ZCL_DATATYPE_CHAR_STR,
+      (ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE),
+      (void *)zclSmartIR_LocationDescription
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_PHYSICAL_ENV,
+      ZCL_DATATYPE_UINT8,
+      (ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE),
+      (void *)&zclSmartIR_PhysicalEnvironment
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_DEVICE_ENABLED,
+      ZCL_DATATYPE_UINT8,
+      (ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE),
+      (void *)&zclSmartIR_DeviceEnable
+    }
+  },
+
+  // *** Identify Cluster Attribute ***
+  {
+    ZCL_CLUSTER_ID_GEN_IDENTIFY,
+    { // Attribute record
+      ATTRID_IDENTIFY_TIME,
+      ZCL_DATATYPE_UINT16,
+      (ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE),
+      (void *)&zclSmartIR_IdentifyTime
+    }
+  },
+/*
+  // *** On / Off Cluster Attributes ***
+  {
+    ZCL_CLUSTER_ID_GEN_ON_OFF,
+    { // Attribute record
+      ATTRID_ON_OFF,
+      ZCL_DATATYPE_BOOLEAN,
+      ACCESS_CONTROL_READ,
+      (void *)&zclSmartIR_OnOff
+    }
+  },
+
+  // *** On / Off Switch Configuration Cluster *** //
+  {
+    ZCL_CLUSTER_ID_GEN_ON_OFF_SWITCH_CONFIG,
+    { // Attribute record
+      ATTRID_ON_OFF_SWITCH_TYPE,
+      ZCL_DATATYPE_ENUM8,
+      ACCESS_CONTROL_READ,
+      (void *)&zclSmartIR_OnOffSwitchType
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_ON_OFF_SWITCH_CONFIG,
+    { // Attribute record
+      ATTRID_ON_OFF_SWITCH_ACTIONS,
+      ZCL_DATATYPE_ENUM8,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclSmartIR_OnOffSwitchActions
+    }
+  },
+  */
+};
