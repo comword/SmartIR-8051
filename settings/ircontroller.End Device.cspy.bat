5ea7d812e8e52d9b2aa5d37f8b3f4b109d335d23
henorvell <comword@live.com>
2016-08-24 20:59:59 +0800

    add .s51

--- settings/ircontroller.End Device.cspy.bat
+++ settings/ircontroller.End Device.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "F:\SmartIR-8051\settings\ircontroller.End device.general.xcl" --backend -f "F:\SmartIR-8051\settings\ircontroller.End device.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "C:\Myfiles\gits\SmartIR-8051\settings\ircontroller.End device.general.xcl" --backend -f "C:\Myfiles\gits\SmartIR-8051\settings\ircontroller.End device.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "F:\SmartIR-8051\settings\ircontroller.End device.general.xcl" "--debug_file=%~1" --backend -f "F:\SmartIR-8051\settings\ircontroller.End device.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.3\common\bin\cspybat" -f "C:\Myfiles\gits\SmartIR-8051\settings\ircontroller.End device.general.xcl" "--debug_file=%~1" --backend -f "C:\Myfiles\gits\SmartIR-8051\settings\ircontroller.End device.driver.xcl" 
 
 @echo off 
 :end
